13b247a6aa7bf5104ffc7c84609f6e0e10a33845
alenjzhang <alenjzhang@tencent.com>
2025-02-13 17:46:28 +0800

    init

---
+++ long-str.js
@@ -0,0 +1,23 @@
+const str = '111222234556789';
+
+function find(str) {
+	let s = '';
+
+	function helper(i) {
+		let result = [];
+		while (i < str.length && !result.includes(str[i])) {
+			result.push(str[i]);
+			i++;
+		}
+		if (s.length < result.length) {
+			s = result.join('');
+		}
+	}
+	for (let i = 0; i < str.length; i++) {
+		helper(i);
+	}
+
+	return s;
+}
+
+console.log(find(str));
