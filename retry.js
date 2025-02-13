809929ec6a2c0bc50b5b9d14983d884da52488ba
alenjzhang <alenjzhang@tencent.com>
2025-02-13 17:07:35 +0800

    feat: retry

--- retry.js
+++ retry.js
@@ -6,25 +6,30 @@ function task() {
 	})
 }
 
-async function retry(fn, times) {
-	let lastError = null;
+function retry(fn, times) {
+	return async (newFn, newTimes) => {
+		newFn = typeof newFn === 'function' ? newFn : fn;
+		newTimes = typeof newTimes === 'number' ? newTimes : times;
+		let lastError = null;
 
-	for (let attempt = 1; attempt <= times; attempt++) {
-		try {
-			console.log('run time', attempt);
-			const result = await fn();
-			return result; // 成功直接返回
-		} catch (err) {
-			console.log(`Attempt ${attempt}/${times} failed`);
-			lastError = err;
-			if (attempt >= times) break;
+		for (let attempt = 1; attempt <= newTimes; attempt++) {
+			try {
+				console.log('run time', attempt);
+				const result = await newFn();
+				return result; // 成功直接返回
+			} catch (err) {
+				console.log(`Attempt ${attempt}/${newTimes} failed`);
+				lastError = err;
+				if (attempt >= newTimes) break;
 
-			// 可以在这里添加重试延迟（可选）
-			// await new Promise(r => setTimeout(r, 1000 * attempt));
+				// 可以在这里添加重试延迟（可选）
+				// await new Promise(r => setTimeout(r, 1000 * attempt));
+			}
 		}
+		throw lastError || new Error('All attempts failed');
 	}
-
-	throw lastError || new Error('All attempts failed');
 }
 
-retry(task, 3)
+// const fn = retry(task, 3);
+
+// fn(task, 2);
